62582418ab391ab6523aff1cabfe7ab3fd573978
LeticiaISilveira <leticia.ingrid.silveira@gmail.com>
2019-09-27 13:34:07 -0300

    tentativa frustrada de separar as funções

--- src/data.js
+++ src/data.js
@@ -7,13 +7,31 @@ const example = () => {
 
 window.example = example;*/
 
-const test = (str) => document.write(str.toUpperCase)
-window.test = test;
+
+app.data = {
+  filter: filter,
+  compareAscending: compareAscending,
+  search: search
+  
+};
+
+//FILTRAR TIPO
+function filter(array, condition) {
+  return array.filter(item => item.type.includes(condition))
+}
+
+/*//ORDENAR NOME
+function filter(array, condition,compareAscending()) {
+  return array.sort(item => item.type.includes(condition))
+}*/
+
+//PESQUISAR NOME
+function search(array, condition) {
+  return array.map(item => item.name.toLowerCase().includes(namePok.toLowerCase()))
+}
 
 
 
- //window.data = filter
- //window.data = order
 
 
 
